eae91557cba1f544cc75ae3c61569e827fb91069
MShafiMS <shahedali734@gmail.com>
2023-12-03 16:20:17 +0600

    added new icons

--- src/components/icons/Burger.tsx
+++ src/components/icons/Burger.tsx
@@ -9,19 +9,19 @@ export const Burger = (props: IIconProps) => {
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M32.5 13.75H7.5V11.25H32.5V13.75Z"
-        fill={props.fill || '#080341'}
+        fill={props.color || '#080341'}
       />
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M32.5 21.25H7.5V18.75H32.5V21.25Z"
-        fill={props.fill || '#080341'}
+        fill={props.color || '#080341'}
       />
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M32.5 28.75H7.5V26.25H32.5V28.75Z"
-        fill={props.fill || '#080341'}
+        fill={props.color || '#080341'}
       />
     </Icon>
   );
