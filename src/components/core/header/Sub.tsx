e7e8ff8afc2ef76ac6d127ba7d547d03c636347f
MCTC <rashelmahmudraj1998@gmail.com>
2023-12-05 01:33:28 +0600

    added sub nav

--- src/components/core/header/Sub.tsx
+++ src/components/core/header/Sub.tsx
@@ -1,11 +1,14 @@
 import { Cart, Love } from '@components/icons';
+import Link from 'next/link';
 
 export const Sub = () => {
   return (
-    <div className="flex  gap-4 py-2 px-40 container mx-auto items-center justify-between lg:px-20 px-4 bg-[#2E2E2E] text-white">
-      <div className="flex gap-2 border-r-2 items-center">
-        <Cart />
-        <label className="mr-20">Phones</label>
+    <div className="lg:flex  gap-4 py-2 container mx-auto items-center justify-between lg:px-20 px-4 bg-[#2E2E2E] text-white text-base">
+      <div className="flex gap-2 border-r-2 items-center ">
+        <Cart className="text-[#2E2E2E]" />
+        <Link href="/">
+          <label className="mr-20">Phones</label>
+        </Link>
       </div>
       <div className="flex border-r-2">
         <Love />
