4b398157bb7363e5afc6cc5e35082e780e7b98af
MCTC <rashelmahmudraj1998@gmail.com>
2023-12-19 21:42:53 +0600

    added

--- src/components/home/Bigsell.tsx
+++ src/components/home/Bigsell.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export function Bigsell() {
   return (
-    <div className="flex justify-between bg-gradient-to-r from-[#2E2E2E] to-[#000000] items-center">
-      <div>
-        <Image width={337.92} height={181.97} src="/images/laptop.png" alt="image" />
-        <Image width={237} height={192} src="/images/pc.png" alt="image" />
-        <Image width={418.56} height={262.08} src="/images/tablet.png" alt="image-2" />
+    <div className="flex justify-between bg-gradient-to-r from-[#2E2E2E] to-[#000000] items-center gap-2 overflow-hidden">
+      <div className="">
+        <Image className="mt-[-300px] ml-44 " width={337.92} height={181.97} src="/images/laptop.png" alt="image" />
+        <Image className="z-10 mt-[-100px]" width={237} height={192} src="/images/pc.png" alt="image" />
+        <Image className="rotate-[180deg] " width={418.56} height={262.08} src="/images/tablet.png" alt="image-2" />
       </div>
-      <div className="text-[#FFFFFF] ">
+      <div className="text-[#FFFFFF] gap-10">
         <h1 className="text-7xl font-extralight mb-3">
           Big Summer <span className="font-medium ">Sale</span>
         </h1>
@@ -18,9 +18,15 @@ export function Bigsell() {
           Shop Now
         </Button>
       </div>
-      <div>
-        <Image height={365} width={120.36} src="/images/i-phone.png" alt="phone" />
-        <Image height={321} width={404} src="/images/watch.png" alt="watch" />
+      <div className=" grid grid-col place-items-end overflow-hidden relative">
+        <Image
+          height={365}
+          width={120.36}
+          className="rotate-[-31.47deg] relative "
+          src="/images/i-phone.png"
+          alt="phone"
+        />
+        <Image className="" height={321} width={404} src="/images/watch.png" alt="watch" />
       </div>
     </div>
   );
