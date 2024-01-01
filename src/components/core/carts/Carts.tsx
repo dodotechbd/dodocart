c6987da3ce7cdf64210be94d9ad946eaea80c6d6
MCTC <rashelmahmudraj1998@gmail.com>
2024-01-01 21:52:13 +0600

    added carts

--- src/components/core/carts/Carts.tsx
+++ src/components/core/carts/Carts.tsx
@@ -1,11 +1,27 @@
+import Link from 'next/link';
 import { ProductCart } from './ProductCart';
 export const Carts = () => {
   return (
-    <div className="container lg:px-40 mx-auto mt-5 grid lg:grid-cols-4 gap-4 ">
-      <ProductCart />
-      <ProductCart />
-      <ProductCart />
-      <ProductCart />
+    <div className="container lg:px-40 mx-auto mt-5 ">
+      <div>
+        <ul className="flex gap-8 mb-8 font-medium text-lg leading-8">
+          <li className="hover:underline">
+            <Link href="#">New Arrival</Link>
+          </li>
+          <li className="hover:underline">
+            <Link href="/cart">Bestseller</Link>
+          </li>
+          <li className="hover:underline">
+            <Link href="#">Featured Products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="grid lg:grid-cols-4 gap-4">
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
+      </div>
     </div>
   );
 };
--- src/components/core/carts/ProductCart.tsx
+++ src/components/core/carts/ProductCart.tsx
@@ -1,8 +1,15 @@
 import { Button } from '@components/core';
+import { Favorite } from '@components/icons';
 import Image from 'next/image';
+import Link from 'next/link';
 export const ProductCart = () => {
   return (
     <div className="py-6 px-4 bg-[#909090C4]  rounded-lg text-center">
+      <div className="flex justify-end">
+        <Link href="#">
+          <Favorite />
+        </Link>
+      </div>
       <Image className="py-4 mx-auto" src="/images/headphone.png" width={160} height={160} alt="cart image" />
       <p className="font-medium leading-6">Apple iPhone 14 Pro Max 128GB Deep Purple (MQ9T3RX/A)</p>
       <h1 className="leading-6 font-semibold">$900</h1>
