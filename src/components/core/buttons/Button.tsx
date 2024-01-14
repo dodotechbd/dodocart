d8b4fc5a9c90479efe8676528ccf965b4f43d31d
MShafiMS <shahedali734@gmail.com>
2024-01-14 21:59:36 +0600

    button updated

--- src/components/core/buttons/Button.tsx
+++ src/components/core/buttons/Button.tsx
@@ -11,11 +11,19 @@ interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button = ({ children, small, light, filled, className, icon, ...rest }: IProps) => {
   const disabled = rest.disabled ? rest.disabled : false;
-  const baseClass = 'rounded-md font-medium border duration-200 flex items-center hover:opacity-80 ';
+  const baseClass = 'rounded-md font-medium border duration-200 flex items-center ';
   const padding = small ? 'py-3 px-16 ' : 'py-4 px-14 ';
   const color = light
-    ? `border-white ${filled ? 'text-black bg-white' : 'text-white bg-white/0'} `
-    : `border-black ${filled ? 'text-white bg-black' : 'text-black bg-black/0'} `;
+    ? `border-white bg-white ${
+        filled
+          ? 'text-black hover:text-white hover:bg-opacity-0'
+          : 'text-white hover:text-black bg-opacity-0 hover:bg-opacity-100'
+      } `
+    : `border-black bg-black ${
+        filled
+          ? 'text-white hover:text-black hover:bg-opacity-0'
+          : 'text-black hover:text-white bg-opacity-0 hover:bg-opacity-100'
+      } `;
   const disabledClass = disabled ? 'opacity-50 cursor-not-allowed ' : 'opacity-100 ';
   const iconColor = light ? (filled ? 'black' : 'white') : filled ? 'white' : 'black';
   const finalClass = baseClass + padding + color + disabledClass + className;
