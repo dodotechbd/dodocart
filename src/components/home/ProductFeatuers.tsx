3fa62e203caa5238b72b76cc2941cfbc899fab48
MCTC <rashelmahmudraj1998@gmail.com>
2024-02-03 09:47:27 +0600

    ade

--- src/components/home/ProductFeatuers.tsx
+++ src/components/home/ProductFeatuers.tsx
@@ -4,17 +4,8 @@ import Image from 'next/image';
 export const ProductFeatuers = () => {
   return (
     <div className="lg:flex">
-      <div className="">
-        <div className="lg:flex place-items-center pr-12 lg:w-[720px]">
-          <Image className="lg:ml-[-110px]" src="/images/gameRemote.png" alt="image" width={400} height={400} />
-          <div className="text-center lg:text-start">
-            <h1 className="font-medium lg:text-5xl text-3xl  py-3">Playstation5</h1>
-            <p className="text-sm text-[#909090] ">
-              Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.
-            </p>
-          </div>
-        </div>
-        <div className="lg:flex ">
+      <div className="lg:flex lg:flex-col">
+        <div className="lg:flex">
           <div className="lg:flex place-items-center bg-[#EDEDED] lg:py-12">
             <Image
               className="lg:ml-[-65px] mx-auto lg:pr-10"
@@ -24,9 +15,9 @@ export const ProductFeatuers = () => {
               alt="headphones"
             />
             <div className="text-center lg:text-start">
-              <h1 className="text-[29px] font-light">Apple</h1>
+              {/* <h1 className="text-[29px] font-light">Apple</h1> */}
               <h1 className="text-[29px] font-light">
-                Vision <span className="font-medium">Pro</span>
+                Apple Vision <span className="font-medium">Pro</span>
               </h1>
               <p className="text-sm text-[#909090]">An immersive way to experience entertainment</p>
             </div>
@@ -47,19 +38,17 @@ export const ProductFeatuers = () => {
             </div>
           </div>
         </div>
+        <div className="lg:flex place-items-center pr-12 lg:order-first lg:w-[720px]">
+          <Image className="lg:ml-[-110px]" src="/images/gameRemote.png" alt="image" width={400} height={400} />
+          <div className="text-center lg:text-start">
+            <h1 className="font-medium lg:text-5xl text-3xl  py-3">Playstation5</h1>
+            <p className="text-sm text-[#909090] ">
+              Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.
+            </p>
+          </div>
+        </div>
       </div>
       <div className="lg:flex place-items-center bg-[#EDEDED] lg:pl-14 p-3">
-        <div className=" mb-3 text-center lg:text-start">
-          <h2 className="font-extralight lg:text-[64px] text-[30px]">
-            Macbook <span className="font-medium">Air</span>
-          </h2>
-          <p className="font-medium text-sm text-[#909090] leading-6">
-            The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.
-          </p>
-          <Button small className="mx-auto lg:mx-0 my-2">
-            Show Now
-          </Button>
-        </div>
         <div className="overflow-hidden">
           <Image
             src="/images/color.png"
@@ -69,6 +58,17 @@ export const ProductFeatuers = () => {
             alt="image"
           />
           <div className="bg-gradient-to-r from-[#7D7E82] from-10% via-[#AFB0B5] via-30% to-[#AEAFB4] to-90% lg:w-96 h-6 lg:ml-20 rounded-b-lg"></div>
+        </div>
+        <div className=" mb-3 text-center lg:text-start order-first">
+          <h2 className="font-extralight lg:text-[64px] text-[30px]">
+            Macbook <span className="font-medium">Air</span>
+          </h2>
+          <p className="font-medium text-sm text-[#909090] leading-6">
+            The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.
+          </p>
+          <Button small className="mx-auto lg:mx-0 my-2">
+            Show Now
+          </Button>
         </div>
       </div>
     </div>
