315b7773a0465fb51264cf7af957cdcf53c0750b
MCTC <rashelmahmudraj1998@gmail.com>
2024-02-12 21:10:04 +0600

    added some carusul

--- src/components/home/PopularProduct.tsx
+++ src/components/home/PopularProduct.tsx
@@ -3,72 +3,108 @@ import Image from 'next/image';
 
 const PopularProduct = () => {
   return (
-    <div className="flex mx-auto my-10 ">
-      <div className=" p-5">
-        <div className="relative">
-          <Image src="/images/boxHeadphone.png" className="rotate-45" height={212} width={250} alt="box headphone" />
-          <Image
-            src="/images/watch2.png"
-            className="-rotate-[120deg] -mt-28 ml-20"
-            height={211}
-            width={250}
-            alt="watch"
-          />
-        </div>
+    <div id="default-carousel" data-carousel="slide">
+      <div className="lg:flex mx-auto my-10 relative h-[38rem] overflow-hidden rounded-lg">
+        <div className="mb-5" data-carousel-item>
+          <div className="relative">
+            <Image src="/images/boxHeadphone.png" className="rotate-45" height={212} width={250} alt="box headphone" />
+            <Image
+              src="/images/watch2.png"
+              className="-rotate-[120deg] -mt-28 ml-20"
+              height={211}
+              width={250}
+              alt="watch"
+            />
+          </div>
 
-        <div className="pl-5 mt-1">
-          <h1 className="text-2xl font-light leading-12">Popular Products</h1>
-          <p className="font-medium text-[#909090] leading-4">
-            iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.
-          </p>
-          <Button className="gap-2 mt-2" small>
-            Shop Now
-          </Button>
+          <div className="pl-5 mt-1">
+            <h1 className="text-2xl font-light leading-12">Popular Products</h1>
+            <p className="font-medium text-[#909090] leading-4">
+              iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of
+              use.
+            </p>
+            <Button className="gap-2 mt-2" small>
+              Shop Now
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className=" p-5 bg-[#F9F9F9]">
-        <div>
-          <Image src="/images/phones.png" className="ml-[80px]" height={471} width={550} alt="box headphone" />
-        </div>
+        <div className=" bg-[#F9F9F9] overflow-hidden" data-carousel-item>
+          <div>
+            <Image src="/images/phones.png" className="ml-[80px]" height={471} width={550} alt="box headphone" />
+          </div>
 
-        <div className="pl-5 mt-4">
-          <h1 className="text-2xl font-light leading-12">Popular Products</h1>
-          <p className="font-medium text-[#909090] leading-4">
-            iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.
-          </p>
-          <Button className="gap-2 mt-7" small>
-            Shop Now
-          </Button>
+          <div className="pl-5  mt-2">
+            <h1 className="text-2xl font-light leading-12">Popular Products</h1>
+            <p className="font-medium text-[#909090] leading-4">
+              iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of
+              use.
+            </p>
+            <Button className="gap-2 mt-3" small>
+              Shop Now
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="p-5 bg-[#EAEAEA]">
-        <div>
-          <Image src="/images/iphone.png" className="mx-auto" height={380} width={320} alt="box headphone" />
-        </div>
+        <div className=" bg-[#EAEAEA]" data-carousel-item>
+          <div>
+            <Image src="/images/iphone.png" className="mx-auto" height={380} width={320} alt="box headphone" />
+          </div>
 
-        <div className="pl-5">
-          <h1 className="text-2xl font-light leading-12">Popular Products</h1>
-          <p className="font-medium text-[#909090] leading-4">
-            iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.
-          </p>
-          <Button className="gap-2 mt-3" small>
-            Shop Now
-          </Button>
+          <div className="pl-5">
+            <h1 className="text-2xl font-light leading-12">Popular Products</h1>
+            <p className="font-medium text-[#909090] leading-4">
+              iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of
+              use.
+            </p>
+            <Button className="gap-2 mt-5" small>
+              Shop Now
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className=" p-5 bg-[#2C2C2C] text-white">
-        <div>
-          <Image src="/images/monitor.png" className="ml-[30%]" height={400} width={900} alt="box headphone" />
-        </div>
+        <div className="py-5 bg-[#2C2C2C] text-white overflow-hidden" data-carousel-item>
+          <div>
+            <Image src="/images/monitor.png" className="ml-[30%]" height={400} width={900} alt="box headphone" />
+          </div>
 
-        <div className="pl-5">
-          <h1 className="text-2xl font-light leading-12">Popular Products</h1>
-          <p className="font-medium text-[#909090] leading-4">
-            iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.
-          </p>
-          <Button className="gap-2 mt-5 text-white border-white" small>
-            Shop Now
-          </Button>
+          <div className="pl-5 lg:mt-16">
+            <h1 className="text-2xl font-light leading-12">Popular Products</h1>
+            <p className="font-medium text-[#909090] leading-4">
+              iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of
+              use.
+            </p>
+            <Button className="gap-2 mt-12 text-white border-white" small>
+              Shop Now
+            </Button>
+          </div>
+        </div>
+        <div className="absolute z-30 lg:hidden flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="w-3 h-3 rounded-full bg-red-600"
+            aria-current="true"
+            aria-label="Slide 1"
+            data-carousel-slide-to="0"
+          ></button>
+          <button
+            type="button"
+            className="w-3 h-3 rounded-full bg-red-600"
+            aria-current="false"
+            aria-label="Slide 2"
+            data-carousel-slide-to="1"
+          ></button>
+          <button
+            type="button"
+            className="w-3 h-3 rounded-full bg-red-600"
+            aria-current="false"
+            aria-label="Slide 3"
+            data-carousel-slide-to="2"
+          ></button>
+          <button
+            type="button"
+            className="w-3 h-3 rounded-full bg-red-600"
+            aria-current="false"
+            aria-label="Slide 4"
+            data-carousel-slide-to="3"
+          ></button>
         </div>
       </div>
     </div>
