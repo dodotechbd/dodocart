2cfc2b6cb3558876c7fffdcf9cb60730ad5b141b
MCTC <rashelmahmudraj1998@gmail.com>
2024-02-16 20:44:06 +0600

    update editing

--- src/components/core/carts/Carts.tsx
+++ src/components/core/carts/Carts.tsx
@@ -4,61 +4,60 @@ export const Carts = () => {
   const [openTab, setOpenTab] = useState(1);
   return (
     <div className="container lg:px-40 px-3 mx-auto mt-5 mb-3 ">
-      <div>
-        <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
-          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-            <a
-              className={
-                'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
-                (openTab === 1 ? 'text-white bg-blueGray-600' : 'text-blueGray-600 bg-white')
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                setOpenTab(1);
-              }}
-              data-toggle="tab"
-              href="#link1"
-              role="tablist"
-            >
-              Profile
-            </a>
-          </li>
-          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-            <a
-              className={
-                'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
-                (openTab === 2 ? 'text-white bg-blueGray-600' : 'text-blueGray-600 bg-white')
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                setOpenTab(2);
-              }}
-              data-toggle="tab"
-              href="#link2"
-              role="tablist"
-            >
-              Settings
-            </a>
-          </li>
-          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-            <a
-              className={
-                'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
-                (openTab === 3 ? 'text-white bg-blueGray-600' : 'text-blueGray-600 bg-white')
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                setOpenTab(3);
-              }}
-              data-toggle="tab"
-              href="#link3"
-              role="tablist"
-            >
-              Options
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
+        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            className={
+              'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+              (openTab === 1 ? 'text-slate-300 bg-blueGray-600' : 'text-blueGray-600 bg-white')
+            }
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenTab(1);
+            }}
+            data-toggle="tab"
+            href="#link1"
+            role="tablist"
+          >
+            New Arrival
+          </a>
+        </li>
+        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            className={
+              'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+              (openTab === 2 ? 'text-slate-300 bg-blueGray-600' : 'text-blueGray-600 bg-white')
+            }
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenTab(2);
+            }}
+            data-toggle="tab"
+            href="#link2"
+            role="tablist"
+          >
+            Bestseller
+          </a>
+        </li>
+        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            className={
+              'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+              (openTab === 3 ? 'text-slate-300 bg-blueGray-600' : 'text-blueGray-600 bg-white')
+            }
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenTab(3);
+            }}
+            data-toggle="tab"
+            href="#link3"
+            role="tablist"
+          >
+            Featured Products
+          </a>
+        </li>
+      </ul>
+
       <div className="grid lg:grid-cols-4 gap-4">
         <div className={openTab === 1 ? 'block' : 'hidden'} id="link2">
           <ProductCart />
